b225f23cb82db83ddaeed0e6c3b37a95bd7b1623
Bilal Budhani <bilalbudhani@gmail.com>
2021-08-19 19:25:18 +0530

    Updates Vultr::Object to support marshalling

--- test/vultr/object_test.rb
+++ test/vultr/object_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class ObjectTest < Minitest::Test
   def test_creating_object_from_hash
-    assert_equal :bar, Vultr::Object.new(foo: :bar).foo
+    assert_equal "bar", Vultr::Object.new(foo: :bar).foo
   end
 
   def test_nested_hash
-    assert_equal :foobar, Vultr::Object.new(foo: {bar: {baz: :foobar}}).foo.bar.baz
+    assert_equal "foobar", Vultr::Object.new(foo: {bar: {baz: :foobar}}).foo.bar.baz
   end
 end
