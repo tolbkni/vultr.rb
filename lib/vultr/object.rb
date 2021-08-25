03d65836153ae6aebe60375baa446111011ff4ec
Chris Oliver <excid3@gmail.com>
2021-08-25 08:36:33 -0500

    Recursively convert to OpenStruct instead of converting to and from JSON redundantly

--- lib/vultr/object.rb
+++ lib/vultr/object.rb
@@ -5,7 +5,14 @@ require "json"
 module Vultr
   class Object < SimpleDelegator
     def initialize(attributes)
-      super(JSON.parse(attributes.to_json, object_class: OpenStruct))
+      super to_recursive_ostruct(attributes)
+    end
+
+    def to_recursive_ostruct(hash)
+      result = hash.each_with_object({}) do |(key, val), memo|
+        memo[key] = val.is_a?(Hash) ? to_recursive_ostruct(val) : val
+      end
+      OpenStruct.new(result)
     end
   end
 end
